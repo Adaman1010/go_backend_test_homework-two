6691c72c56f5b65abb2e7088448cd96a410877a2
aybika <adamovaaybika@mail.ru>
2026-02-01 19:50:32 +0300

    Текст вашего комментария

---
+++ main.go
@@ -0,0 +1,17 @@
+package main
+
+//123
+// не хочу учиться
+///kjhkjhkj
+// неужели это првда?
+import (
+	"fmt"
+)
+
+func Add(a, b int) int {
+	return a + b
+}
+
+func main() {
+	fmt.Println("Я домашка")
+}
